cd9c3fc62e8f4b42d1befaf9fb6304ce692f31ba
ciupa <ciupaandrei123@Icloud.com>
2025-02-04 15:24:57 +0200

    23456789

--- TeamCode/src/main/java/pedroPathing/TeleOpPID.java
+++ TeamCode/src/main/java/pedroPathing/TeleOpPID.java
@@ -41,6 +41,23 @@ public class TeleOpPID extends OpMode {
     public static double lf = 0;
     public static int ltarget = 0;
     private final double ticks_in_mm = 3.20;
+
+    private PIDController hang1pid;
+    public static double h1p = 0, h1i = 0, h1d = 0;
+    public  static  double h1f = 0;
+
+    public static int h1target = 0 ;
+
+    private  final double h1ticks_in_degree = 3.434;
+
+    private PIDController hang2pid;
+    public static double h2p = 0, h2i = 0, h2d = 0;
+    public  static  double h2f = 0;
+
+    public static int h2target = 0 ;
+
+    private  final double h2ticks_in_degree = 3.434;
+
     private DcMotorEx  motor_stanga         = null; //the arm motor
     private DcMotorEx  motor_glisiere        = null; //
     private DcMotorEx fata_stanga = null;
@@ -59,6 +76,9 @@ public class TeleOpPID extends OpMode {
 
         controller = new PIDController(p, i, d);
         liftcontroller = new PIDController(lp, li, ld);
+        hang1pid = new PIDController(h1p, h1i, h1d);
+        hang2pid = new PIDController(h2p, h2i, h2d);
+
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
@@ -104,14 +124,30 @@ public class TeleOpPID extends OpMode {
     public void loop() {
         controller.setPID(p,i,d);
         liftcontroller.setPID(lp,li,ld);
+
         int armPos = motor_stanga.getCurrentPosition();
         int liftPos = motor_glisiere.getCurrentPosition();
+        int hang1Pos = hang1.getCurrentPosition();
+        int hang2Pos = hang2.getCurrentPosition();
+
+        hang1pid.setPID(h1p, h1i, h1d);
+        hang2pid.setPID(h2p, h2i, h2d);
+
         double pid = controller.calculate(armPos, target);
         double liftpid = liftcontroller.calculate(liftPos, ltarget);
+        double h1pid = controller.calculate(hang1Pos, h1target);
+        double h2pid = controller.calculate(hang2Pos, h2target);
+
         double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
         double liftff = Math.cos(Math.toRadians(ltarget / ticks_in_mm)) * lf;
+        double h1ff = Math.cos(Math.toRadians(h1target / h1ticks_in_degree)) * h1f;
+        double h2ff = Math.cos(Math.toRadians(h2target / h2ticks_in_degree)) * h2f;
+
         double power = pid + ff;
         double liftpower = liftpid + liftff;
+        double h1power = h1pid + h1ff;
+        double h2power = h2pid + h2ff;
+
 
         double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
         double x = gamepad1.left_stick_x ; // Counteract imperfect strafing
@@ -133,6 +169,10 @@ public class TeleOpPID extends OpMode {
 
         motor_stanga.setPower(power);
         motor_glisiere.setPower(liftpower);
+        hang1.setPower(h1power);
+        hang2.setPower(h2power);
+
+
         telemetry.addData("pos arm", armPos);
         telemetry.addData("lift pos", liftPos);
         telemetry.update();
